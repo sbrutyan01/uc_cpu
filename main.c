f953f15c011bad9bd7b85ed16bad7aba5501327a
Sergey Brutyan <sbrutyan01@gmail.com>
2026-04-21 00:12:05 +0400

    AND gate implementation

--- main.c
+++ main.c
@@ -2,9 +2,36 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define NIBBLE  4
+#define BYTE    8
+#define WORD    16
+#define DWORD   32
+
+typedef enum { false, true } bool;
+
+bool wire[NIBBLE];
+
+void and(bool a[NIBBLE], bool b[NIBBLE]);
+
 int main(void)
 {
-    printf("Hello World\n");
+    // bool a = true
+    bool a[NIBBLE] = { true, true, true, true };
+
+    // bool b = true
+    bool b[NIBBLE] = { true, true, true, true };
+
+    and(a, b);
+
+    for (int i = 0; i < NIBBLE; i++ ) {
+        printf("Y is: %d\n", wire[i]);
+    }
 
 return 0;
+}
+
+void and(bool a[NIBBLE], bool b[NIBBLE]) {
+    for (int i = 0; i < NIBBLE; i++) {
+        wire[i] = a[i] & b[i];
+    }
 }
